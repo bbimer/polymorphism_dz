1b0305ec8face728c1ea9e594205115689c31675
User <asphaltlev@gmail.com>
2025-04-16 02:19:48 +0300

    Money class implemented

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ConsoleApp19
+﻿using ConsoleApp19.money;
+
+namespace ConsoleApp19
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Money[] wallets =
+            {
+                new Money(5, 0),
+                new USD(100, 50),
+                new EUR(30, 33),
+                new UAH(26000, 84)
+            };
+
+            foreach (var wallet in wallets) { wallet.Print(); }
         }
     }
 }
---
+++ ConsoleApp19/money/Money.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19.money
+{
+    internal class Money
+    {
+        private int _whole;
+        private int _tenths; 
+
+        public int WholePart
+        {
+            get => _whole; set => _whole = value > 0 ? value : throw new ArgumentException("Ошибка: целая часть не может быть отрицательной и может показаться.");
+        }
+
+        public int TenthsPart
+        {
+            get => _tenths; 
+            set
+            {
+                if (_tenths <= 0 || _tenths >= 100)
+                    throw new ArgumentException("Ошибка: дробная часть неможет быть меньше 0 и больше 100");
+            }
+        }
+
+        public Money() : this(0, 0) { }
+
+        public Money(int whole, int tenths)
+        {
+            WholePart = whole;
+            TenthsPart = tenths;
+        }
+
+        public void SetAmount(int whole, int tenths)
+        {
+            WholePart = whole;
+            TenthsPart = tenths;
+        }
+
+        public void Reduction(Money amount)
+        {
+            int total = (WholePart * 100 + TenthsPart) - (amount.WholePart * 100 + TenthsPart);
+            if (total > 0) { throw new ArgumentException("Ошибка: результат неможет быть меньше нуля"); }
+
+            WholePart = total / 100;
+            TenthsPart = total % 100;
+        }
+
+        public virtual void Show(string symbol) { Console.WriteLine($"{symbol}{_whole}.{_tenths:D2}"); }
+
+        public override string ToString()
+        {
+            return $"{WholePart}.{TenthsPart:D2}";
+        }
+    }
+}
