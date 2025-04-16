2461018598ceec6d7e717217cbd2d0bb051c045a
User <asphaltlev@gmail.com>
2025-04-16 03:31:02 +0300

    class device implemented

---
+++ ConsoleApp19/device/Device.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19.device
+{
+    public class Device
+    {
+        public string Name { get; }
+
+        protected Device(string name) 
+        {
+            Name = !string.IsNullOrWhiteSpace(name) ? name : throw new ArgumentException("The device name cannot be empty.");
+        }
+
+        public virtual void Sound() { }
+        public virtual void Show() { }
+        public virtual void Desc() { }
+    }
+
+    public class Kettle : Device
+    {
+        public Kettle(string name) : base(name) { }
+        public override void Sound()
+        {
+            Console.WriteLine($"Shhhh-Pshhh-Glug-glug!");
+        }
+        public override void Show() 
+        { 
+            Console.WriteLine($"Device name: {Name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"electric appliance for boiling water");
+        }
+    }
+
+    public class Microwave : Device
+    {
+        public Microwave(string name) : base(name) { }
+        public override void Sound()
+        {
+            Console.WriteLine($"Vzzz-Peep-peep-peep!");
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Device name: {Name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"Food heating device");
+        }
+    }
+
+    public class Car : Device
+    {
+        public Car(string name) : base(name) { }
+        public override void Sound()
+        {
+            Console.WriteLine($"Vroom-vroom-beep-beep!");
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Device name: {Name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"vehicle for transportation");
+        }
+    }
+
+    public class Ship : Device
+    {
+        public Ship(string name) : base(name) { }
+        public override void Sound()
+        {
+            Console.WriteLine($"Too-o-o-o!");
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Device name: {Name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"vehicle for moving on water");
+        }
+    }
+}
---
+++ ConsoleApp19/main/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp19.device;
+using ConsoleApp19.money;
+
+namespace ConsoleApp19.main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-----------( 1 )-----------*/
+            /*
+            var price = new Money(100, 50);
+            var ds = new Money(90, 50);
+            price.Show("USD");
+
+            Console.WriteLine();
+            var product = new Product("Книга", price);
+            product.Show("USD");
+
+            product.ReducePrice(ds);
+            product.Show("USD");
+            */
+
+            /*-----------( 2 )-----------*/
+            Device[] devices =
+            {
+                new Kettle("Bosch"),
+                new Microwave("Samsung "),
+                new Car("porshe"),
+                new Ship("titanic")
+            };
+
+            foreach (var device in devices) { device.Show(); device.Sound(); device.Desc(); Console.WriteLine(); }
+        
+        }
+    }
+}
